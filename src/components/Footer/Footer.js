59a992e8937df36b86872b44683b4cb78c38a30f
engineer-utkarsh <utkarshmoney437@gmail.com>
2025-09-25 13:51:54 +0530

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,8 +8,9 @@ const Footer = () => {
         <a href="https://github.com" aria-label="GitHub">GitHub</a>
         <a href="https://linkedin.com" aria-label="LinkedIn">LinkedIn</a>
         <a href="https://twitter.com" aria-label="Twitter">Twitter</a>
+        <p>&copy; {new Date().getFullYear()} Utkarsh Shukla. All rights reserved.</p>
+
       </div>
-      <p>&copy; {new Date().getFullYear()} Utkarsh Shukla. All rights reserved.</p>
     </footer>
   );
 };
